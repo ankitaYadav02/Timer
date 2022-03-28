401e49e772defc025379254b5953c9106829c133
Ankita Yadav <ankitarao01011998@gmaail.com>
2022-03-28 23:50:58 +0530

    Setup web socket

--- server.js
+++ server.js
@@ -1,7 +1,22 @@
 import express from "express";
-const PORT = 3000;
-const server = express();
+import http from "http";
+import { Server } from "socket.io";
+import path from "path";
 
-server.listen(PORT,()=>{
+const __dirname = path.resolve();
+const PORT = 8000;
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+  });
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+  })
+
+app.listen(PORT,()=>{
     console.log(`server listening on port ${PORT}`);
 })
